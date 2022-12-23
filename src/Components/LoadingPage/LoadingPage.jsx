58e0944fdde2ed2f4f4038919af1d95c587c69d8
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-12-23 19:59:07 +0600

    loading color change to yellow

--- src/Components/LoadingPage/LoadingPage.jsx
+++ src/Components/LoadingPage/LoadingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingPage = () => {
     return (
-        <div className="w-40 h-40 m-auto my-10  border-dashed  border-red-600 rounded-full animate-spin dark:border-violet-400 text-teal-500 border-4"></div>
+        <div className="w-40 h-40 m-auto my-10  border-dashed  border-yellow-300 rounded-full animate-spin dark:border-violet-400 text-teal-500 border-4"></div>
     );
 };
 
